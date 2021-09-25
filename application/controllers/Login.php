2a6ac88095eabaa9edcf0d1a32ffccccdddae814
Su LAN <s.lan@uqconnect.edu.au>
2021-09-25 15:44:36 +1000

    D

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -49,31 +49,6 @@ class login extends CI_Controller {
 		
 				
 
-			if($captcha_response != '')
-			{
-				$keySecret = '6LcuztkaAAAAAHSI0acti6Gwnav7cBG8R2KEWpR0';
-	
-				$check = array(
-					'secret'		=>	$keySecret,
-					'response'		=>	$this->input->post('g-recaptcha-response')
-				);
-	
-				$startProcess = curl_init();
-	
-				curl_setopt($startProcess, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-	
-				curl_setopt($startProcess, CURLOPT_POST, true);
-	
-				curl_setopt($startProcess, CURLOPT_POSTFIELDS, http_build_query($check));
-	
-				curl_setopt($startProcess, CURLOPT_SSL_VERIFYPEER, false);
-	
-				curl_setopt($startProcess, CURLOPT_RETURNTRANSFER, true);
-	
-				$receiveData = curl_exec($startProcess);
-	
-				$finalResponse = json_decode($receiveData, true);
-	
 				if($finalResponse['success'])
 				{
 					if (password_verify($password,$hash))//check username and password
@@ -97,11 +72,6 @@ class login extends CI_Controller {
 					$this->session->set_flashdata('message', 'Validation Fail Try Again');
 					redirect('login');
 				}
-			}
-			else{
-				$this->session->set_flashdata('message', 'Validation Fail Try Again');
-				redirect('login');
-			}
 
 
 
