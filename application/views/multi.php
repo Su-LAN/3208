186d0ee679a8d52ff625c879059de6793369fcf0
Su LAN <s.lan@uqconnect.edu.au>
2021-10-12 19:12:55 +1000

    D

--- application/views/multi.php
+++ application/views/multi.php
@@ -1,6 +1,6 @@
 
 <div class="container">
-    <?php echo form_open_multipart("upload/multiple_upload"); ?>
+    <?php echo form_open_multipart("index.php/upload/multiple_upload"); ?>
     <div class="col-6 col-md-4">
         Upload multiple files at the same time
         <input type="file" name="userfile[]" multiple size="20" class="file">     
