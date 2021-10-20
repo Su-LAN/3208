65fa40b584679eab2c6b2c2b2721db32b2671f9c
Su LAN <s.lan@uqconnect.edu.au>
2021-10-20 12:25:34 +1000

    D

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -73,7 +73,7 @@ class Upload extends CI_Controller
 				 $_FILES['new_file']['size'] = $_FILES['userfile']['size'][$i];
 				 $config['upload_path'] = '/var/www/html/proj/uploads/';
 				 $config["upload_path"] = './uploads/';
-				 //$config["allowed_types"] = "jpg|png|gif|png|mp4|JPG]PNG";
+				 $config["allowed_types"] = "jpg|png|gif|png|mp4|JPG]PNG";
 				 $config["max_size"] = 10000;
 				 $config["max_width"] = 10240;
 				 $config["max_height"] = 7600;
